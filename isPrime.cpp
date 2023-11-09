70bd0f7ef529237875a996a7217b975116a5f48a
petar-Atanasov <petarattanasov@gmail.com>
2023-11-09 09:40:56 +0000

    Second Commit

--- isPrime.cpp
+++ isPrime.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
+using namespace std;
 
-int main(){
+int main()
+{
 
   int number;
-  std::cout << "Enter an integer: ";
-  std::cin >> number;
+  cout << "Enter an integer: ";
+  cin >> number;
 
   // test divisors of number, if a divisor other than 1 and number is
   // found, then number is not prime.
-  bool isPrime = true;
-  for (int i = 2; i <= number; i++)
-    if (!(number % i))
-      isPrime = false;
 
-  if (isPrime)
-    std::cout << "Prime\n";
-  else
-    std::cout << "Not prime\n";
-  
+  bool isPrime = true;
+  if (number >= 2){
+    for (int i = 2; i < number; i++){
+      if (number % i == 0){
+        isPrime = false;
+        break;
+      }
+      if (i == number - 1){
+        isPrime = true;
+      }
+    }
+  } else {
+    isPrime = false;
+    }
+    if (isPrime) {
+    cout << number << " is prime\n";
+  } else {
+    cout << number << " is not prime\n";
+  }
+
   return 0;
 }
